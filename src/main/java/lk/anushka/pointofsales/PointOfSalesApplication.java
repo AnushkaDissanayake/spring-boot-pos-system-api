5128a889a45e3aabc575a48a568aa1b8d0507f2c
Anushka Dissanayake <d.anushka@gtngroup.com>
2023-03-19 17:23:41 +0530

    Initial Commit

---
+++ src/main/java/lk/anushka/pointofsales/PointOfSalesApplication.java
@@ -0,0 +1,13 @@
+package lk.anushka.pointofsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/anushka/pointofsales/PointOfSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.anushka.pointofsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
